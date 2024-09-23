a9f7804ee1636241b5fce27594214d76e87a7062
reza-farel-ramdhani <rezafarel20@gmail.com>
2024-09-23 10:56:26 +0700

    Add files via upload

---
+++ Tugas 1a.py
@@ -0,0 +1,14 @@
+## Reza Farel Ramdhani (122703002*)
+## Perhitungan soal fisika berbasis python
+## Tugas
+
+# No.1a
+# 1/f = (n-1)*(1/R1+1/R2)
+n = 1.5
+R1 = 22 #cm
+R2 = 17.5 #cm
+
+# Masukkan ke rumus untuk mencari f
+f = 1/((n-1)*(1/R1+1/R2))
+
+print("Jarak fokus lensa : ",f, "cm")
